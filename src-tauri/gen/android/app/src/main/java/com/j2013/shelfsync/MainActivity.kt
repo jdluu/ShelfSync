31a6d5938d6b05c1bea071ba9b93f9937e9c1fd2
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2026-01-13 17:00:01 -0800

    chore: initial project setup Initialize project with pnpm, Tauru, React, and android build support

---
+++ src-tauri/gen/android/app/src/main/java/com/j2013/shelfsync/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.j2013.shelfsync
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
