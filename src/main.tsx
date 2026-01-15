0737d40bc96b4ded5aa1e846829d650397707f4a
jdluu <j2013agl@gmail.com>
2026-01-15 15:27:38 -0800

    feat(theme): implement Nord color palette
    Creates custom Chakra UI theme with all 16 Nord colors mapped to semantic color scales. Replaces default gray, blue, green, red, orange, yellow, and purple palettes with Nord equivalents.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { nordSystem } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={nordSystem}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
