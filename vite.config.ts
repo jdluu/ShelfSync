31c6bf89a8cb08fa1268686574f687d2476f279a
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2026-02-11 17:40:14 -0800

    chore(build): migrate to Biome and refine project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/postcss";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
